5ce8a889977170235e164b6a47fc3eb281288355
suhas salunke <suhasnsalunke>
2020-10-15 17:43:50 +0530

    Updates

---
+++ precache-manifest.4b7eadba73be1bbdf39c991d69a1950d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c97e2572f53e23375a604854124e13",
+    "url": "/myreactfirstsite/index.html"
+  },
+  {
+    "revision": "7fc6e8cd8d6ad57c18ab",
+    "url": "/myreactfirstsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "d82b4c72a1b0d4f608e5",
+    "url": "/myreactfirstsite/static/css/main.c2e82780.chunk.css"
+  },
+  {
+    "revision": "7fc6e8cd8d6ad57c18ab",
+    "url": "/myreactfirstsite/static/js/2.a2886420.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/myreactfirstsite/static/js/2.a2886420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82b4c72a1b0d4f608e5",
+    "url": "/myreactfirstsite/static/js/main.f453918e.chunk.js"
+  },
+  {
+    "revision": "c145c46b3a002c282122",
+    "url": "/myreactfirstsite/static/js/runtime-main.e6571d3f.js"
+  },
+  {
+    "revision": "619d9fb13f64d429a532297e15d6b466",
+    "url": "/myreactfirstsite/static/media/a1.619d9fb1.png"
+  },
+  {
+    "revision": "2927e5bc78df5318567a740e9327c4b8",
+    "url": "/myreactfirstsite/static/media/s1.2927e5bc.jpg"
+  }
+]);
